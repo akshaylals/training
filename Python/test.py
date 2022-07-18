bb5b88a48163e704c00bb35c5aec7689fb41e6d3
akshaylals <akshaylal.s@inapp.com>
2022-07-18 12:39:25 +0530

    string format

---
+++ Python/test.py
@@ -0,0 +1,26 @@
+import sys
+print(sys.version)
+print('Hello, World!')
+
+# % formatting
+make = 'Dell'
+dollarRate = 70.256
+myText = 'the amount for this %s computer is %d USD and the exchange rate is %4.2f USD to 1 INR' %(make, 1299, dollarRate)
+print(myText)
+
+# Formatting using format() method
+myText = 'The amount for this {} computer is {} USD and the exchange rate is {} USD to 1 INR'.format(make, 1299, dollarRate)
+print(myText)
+
+myText = '{0} is easier than {1}'.format('Python', 'Java')
+print(myText)
+
+# count the entire string 
+print('Hello Good Morning'.count('d'))
+
+# count from index 3 to end of string
+print('Hello Good Morning'.count('d', 3))
+
+# count from index 3 to 10-1
+print('Hello Good Morning'.count('d', 3, 13))
+
