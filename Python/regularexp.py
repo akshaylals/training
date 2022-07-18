d972ab1e127421aa54eef62f812ebe8f29e35355
akshaylals <akshaylal.s@inapp.com>
2022-07-18 14:48:54 +0530

    findall split sub

--- Python/regularexp.py
+++ Python/regularexp.py
@@ -7,3 +7,20 @@ print(x)
 print(x.span())
 print(x.string)
 print(x.group())
+
+x = re.findall('bi', txt)
+print(x)
+
+x = re.split(' ', txt)
+print(x)
+
+x = re.sub(' ', '-', txt)
+print(x)
+
+txt = 'Hello world'
+x = re.findall('[a-m]', txt)
+print(x)
+
+txt = 'James bond is 007'
+x = re.findall('[0-9]', txt)
+print(x)
