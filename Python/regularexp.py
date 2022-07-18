ae5be9a8c9351093510f365162c8d8854ccadb90
akshaylals <akshaylal.s@inapp.com>
2022-07-18 15:49:49 +0530

    begining and end

--- Python/regularexp.py
+++ Python/regularexp.py
@@ -18,9 +18,36 @@ x = re.sub(' ', '-', txt)
 print(x)
 
 txt = 'Hello world'
-x = re.findall('[a-m]', txt)
+x = re.findall(r'[a-m]', txt)
 print(x)
 
 txt = 'James bond is 007'
-x = re.findall('[0-9]', txt)
+x = re.findall(r'[0-9]', txt)
 print(x)
+
+x = re.findall(r'J...s', txt)
+print(x)
+
+x = re.findall(r'J[a-z]*s', txt)
+print(x)
+
+# look if the string starts with bond
+x = re.findall(r'^James', txt)
+print(x)
+x = re.findall(r'\AJames', txt)
+print(x)
+
+# look if the string ends with bond
+x = re.findall(r'007$', txt)
+print(x)
+
+# look if the word starts with bond
+x = re.findall(r'\bbond', txt)
+print(x)
+
+# matching an email
+txt = 'hello test@gmail.com how are you?'
+# check for non space chars before and after '@'
+x = re.findall(r'\S+@\S+', txt)
+print(x)
+
