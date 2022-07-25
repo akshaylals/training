212cc53540effa2956604e2a9941538c6f771a20
akshaylals <akshaylal.s@inapp.com>
2022-07-25 10:42:34 +0530

    map filter reduce

--- Python/map_filter_reduce.py
+++ Python/map_filter_reduce.py
@@ -18,8 +18,8 @@ def myMapFn(a):
 x = map(myMapFn, (1, 2, 3, 4, 5))
 print(tuple(x))
 
-# A simple program to input numbers and print squares
-print('\n'.join(tuple(map(lambda a, p = int(input('Enter power: ')): f'{a} ^ {p} = {int(a) ** p}', input('Enter list of numbers: ').split()))))
+# A simple program to input numbers and power and print powers
+# print('\n'.join(tuple(map(lambda a, p = int(input('Enter power: ')): f'{a} ^ {p} = {int(a) ** p}', input('Enter list of numbers: ').split()))))
 
 
 
@@ -30,3 +30,15 @@ def func(x):
 
 y = filter(func, (1, 2, 3, 4))
 print(list(y))
+
+print(list(filter(lambda x: x >= 3, (1, 2, 3, 4))))
+
+
+# Reduce fn
+from functools import reduce
+x = reduce(lambda a, b: a + b, [23, 21, 45, 98])
+print(x)
+
+# sum numbers between a range from input
+print('Sum:', reduce(lambda a, b: a + b, filter(lambda x: 3 < x < 9, map(int, input('Enter numbers: ').split()))))
+
