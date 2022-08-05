c1f386b0d9012b94b2fffe3ea55d6fb40481f14d
akshaylals <akshaylal.s@inapp.com>
2022-08-05 10:00:47 +0530

    json

---
+++ html/json.js
@@ -0,0 +1,11 @@
+var jsonString = JSON.stringify({
+    name: 'Abhi',
+    age: 30,
+    address: {
+        district: 'TVM',
+        location: 'Technopark'
+    }
+});
+
+console.log(jsonString);
+
