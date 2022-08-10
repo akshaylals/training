4c4c815584ef932759525aec855ce4348600622b
akshaylals <akshaylal.s@inapp.com>
2022-08-10 17:12:50 +0530

    bundle.js

--- html/mdnpm/webpack.config.js
+++ html/mdnpm/webpack.config.js
@@ -1,13 +1,22 @@
+const autoprefixer = require('autoprefixer')
+
 module.exports = [{
   mode: 'production',
-  entry: './app.scss',
+  entry: ['./app.scss', './app.js'],
   output: {
     // This is necessary for webpack to compile
     // But we never use style-bundle.js
-    filename: 'style-bundle.js',
+    filename: 'bundle.js',
   },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
       {
         test: /\.scss$/,
         use: [
@@ -20,6 +29,16 @@ module.exports = [{
           { loader: 'extract-loader' },
           { loader: 'css-loader' },
           {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  autoprefixer()
+                ]
+              }
+            }
+          },
+          {
             loader: 'sass-loader',
             options: {
               // Prefer Dart Sass
@@ -27,6 +46,9 @@ module.exports = [{
 
               // See https://github.com/webpack-contrib/sass-loader/issues/804
               webpackImporter: false,
+              sassOptions: {
+                includePaths: ['./node_modules']
+              }
             },
           },
         ]
