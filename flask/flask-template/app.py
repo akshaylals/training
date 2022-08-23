bd29bac6aae1e810cc40b8e33bdd9c7994471c83
akshaylals <akshaylal.s@inapp.com>
2022-08-23 17:20:27 +0530

    flask template

---
+++ flask/flask-template/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from flask_material import Material
+
+app = Flask(__name__)
+Material(app)
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template(
+        'user.html',
+        username = name,
+        n = 3
+    )
+
+
+@app.route('/material/<name>')
+def material(name):
+    return render_template(
+        'material.html',
+        username = name,
+        n = 3
+    )
