776ddd1f0cfbdde3c7ee526c701860169506acc7
akshaylals <akshaylal.s@inapp.com>
2022-08-24 12:43:02 +0530

    wtf form file upload

--- flask/wtf/app.py
+++ flask/wtf/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 
 from myformclass import NameForm
 
@@ -13,3 +14,16 @@ def wtf():
         return render_template('wtf.html', form=NameForm(), name=request.form.get('name'))
     else:
         return render_template('wtf.html', form=NameForm())
+
+
+@app.route('/enquiry', methods=['GET', 'POST'])
+def enquiry():
+    form = NameForm()
+    name = None
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        filename = secure_filename(form.filename.data.filename)
+        form.filename.data.save('uploads/' + filename)
+        
+    return render_template('wtf.html', form=form, name=name)
--- flask/wtf/myformclass.py
+++ flask/wtf/myformclass.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_wtf.file import FileField
 
 
 class NameForm(FlaskForm):
     name = StringField('Enter your name', validators=[DataRequired()])
     submit = SubmitField('Submit')
+    filename = FileField("Upload your profile pic")
